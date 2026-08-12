36bfbaf072c82d9be49529444417607002952f86
RSOB-RQM <mpuranik@amazon.com>
2026-08-12 07:00:25 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['xlsx'],
-  },
+  serverExternalPackages: ['xlsx'],
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
